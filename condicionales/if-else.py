97420dbc1fe6d63d2cf7b31600e1fd4196d587c5
Juanse1108 <juansetonog@gmail.com>
2024-07-31 20:13:19 -0500

    metodo slice y condicionales

---
+++ condicionales/if-else.py
@@ -0,0 +1,8 @@
+contrasena_almacenada = "JheysonGalvis"
+contrasena_escrita = "JheysonGalvis"
+
+if contrasena_almacenada == contrasena_escrita:
+    print("hay coincidencia")
+else:
+    print("No hay conincidencia")
+
