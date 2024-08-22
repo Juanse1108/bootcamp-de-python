3f56ad0a12906512351891b89153df8db65dc8a0
Juanse1108 <juansetonog@gmail.com>
2024-08-21 19:30:19 -0500

    abordamos inputs

---
+++ inputs/input_numeros.py
@@ -0,0 +1,6 @@
+#Pedirle un número al usuario
+numero = input("Dame un número campista: ")
+#Multiplicarlo por 2
+resultado = int(numero) * 2
+#Mostrar el resultado
+print(f'{numero} * 2 es: {resultado}')
---
+++ inputs/inputs.py
@@ -0,0 +1,5 @@
+#Pedirle un dato al usuario
+nombre = input("¿Cual es tu nombre?: ")
+#mostrar el dato
+print(f'El nombre es {nombre}')
+print(type(nombre))
